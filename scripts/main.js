2b99292a21e81315924c6ff233bc62af1946d793
ggoodkey <ggoodkey@hotmail.com>
2025-05-12 18:03:28 -0600

    display output

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,21 @@
 import MakerJs from "makerjs";
 
-var models = {};
-models.curve = {
+var output = document.getElementById('output');
+var svgOutput = document.getElementById('svg');
+
+var curves = {
   models:{
     curve1: new MakerJs.models.BezierCurve([0,0],[25,35],[30,-10],[50,0]),
-	curve2: new MakerJs.models.BezierCurve([50,0],[75,35],[80,-10],[100,0])
+	  curve2: new MakerJs.models.BezierCurve([50,0],[75,35],[80,-10],[100,0])
   }
 };
 
-MakerJs.model.findChains(models.curve, function (chains, loose, layer, ignored) {
+MakerJs.model.findChains(curves, function (chains, loose, layer, ignored) {
   for (var a = 0; a < chains.length; a++) {
     for (var b = 0; b < chains[a].links.length; b++) {
-      console.log(chains[a].links[b].walkedPath.pathContext.type);
+      output.innerHTML += '<li>path type: ' + chains[a].links[b].walkedPath.pathContext.type + '</li>';
     }
   }
 }.bind(this), { unifyBeziers: true });
+
+svgOutput.innerHTML = MakerJs.exporter.toSVG(curves, { scale: 2, origin: [50,25] });
